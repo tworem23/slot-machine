6a3628a836e604bab7e92d059e8e6b537e0c9614
Josh <josh.remington1997@gmail.com>
2023-11-09 16:24:34 -0800

    add html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 // state variables 
 //the three slots will be random int
-
+let slot1 = 0;
+let slot2 = 0;
+let slot3 = 0;
 // declare a message 
+let msg;
 
+console.log(slot1, slot2, slot3);
 // cached elements
 // the three slots will be assigned to my bubbles on the web browser
-
+document.getElementById('slot1').innerText = slot1;
+document.getElementById('slot2').innerText = slot2;
+document.getElementById('slot3').innerText = slot3;
 
 // assign message element 
+document.querySelector('h4').innerText = msg;
 
 //assign button element
-
+document.querySelector('button') = spinBtn;
 
 // assign each int to a img/symbol
 
