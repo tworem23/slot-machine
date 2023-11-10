2ccfe73dfc306488575bde5332fc8e97aee66029
Josh <josh.remington1997@gmail.com>
2023-11-09 19:26:50 -0800

    clean up pseudocode and finish logic

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
-// state variables 
+/*----state variables----*/
 //the three slots will be random int
 let slot1 = randomInt();
 let slot2 = randomInt();
 let slot3 = randomInt();
-// declare a message 
-let spinBtn = document.querySelector('button');
-
-console.log(slot1, slot2, slot3);
-
-// assign message element 
-// document.querySelector('h4').innerText = msg;
 
 //assign button element
-spinBtn =
+let spinBtn = document.querySelector('button');
 
+/*-----cached elements----*/
 // assign each int to a img/symbol
 
-// event listener
-spinBtn.addEventListener('click', spin);
+
+
+
+/*----event listener----*/
 //click the spin button to play again 
+spinBtn.addEventListener('click', spin);
+
+/*------functions-----*/
+// spin reiterates the random function for each slot
 function spin() {
     slot1 = randomInt();
     slot2 = randomInt();
     slot3 = randomInt();
-    checkWinner();
     render();
 }
 
@@ -33,24 +32,30 @@ function randomInt() {
     // console.log(randomInt())
 }
 
+// assigns the random states and message to the DOM 
 function render() {
-    // the three slots will be assigned to my bubbles on the web browser
     document.getElementById('slot1').innerText = slot1;
     document.getElementById('slot2').innerText = slot2;
     document.getElementById('slot3').innerText = slot3;
-    checkWinner();
+    message();
 }
 
-function checkWinner() {
-    // when you get three matching symbols the message will read Winner!
-    if (slot1 === slot2 === slot3) {
-        return document.querySelector('h4').innerText = 'Winner!';
+//renders message using boolean function
+function message() {
+    // when you get three matching symbols the message will read "Winner!"
+    if (slot1 === slot2 && slot2 === slot3) {
+        document.querySelector('h4').innerText = 'Winner!';
         console.log('Winner!')
 
-        // when you don't have three matching symbols the message will read Spin Again!
+    // when three symbols don't match the message will read "Spin Again!"
     } else {
-        return document.querySelector('h4').innerText = 'Spin Again!';
+        document.querySelector('h4').innerText = 'Spin Again!';
         console.log('Spin Again!')
     }
 }
-// render();
+
+
+
+
+
+//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
