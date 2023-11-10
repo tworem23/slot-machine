cb8e700bf5c62afc431bec1e8e354692eb9a7073
Josh <josh.remington1997@gmail.com>
2023-11-09 16:15:01 -0800

    add boilerplate and pseudo code

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// state variables 
+//the three slots will be random int
+
+// declare a message 
+
+// cached elements
+// the three slots will be assigned to my bubbles on the web browser
+
+
+// assign message element 
+
+//assign button element
+
+
+// assign each int to a img/symbol
+
+
+// event listener
+//click the spin button to play again 
+
+
+// functions
+// when you get three matching symbols the message will read Winner!
+// when you don't have three matching symbols the message will read Spin Again!
