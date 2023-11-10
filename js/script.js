a1454b439b855c1340ca5077a77042b0328fcee2
Josh <josh.remington1997@gmail.com>
2023-11-09 17:58:42 -0800

    testing random ints

--- js/script.js
+++ js/script.js
@@ -1,31 +1,49 @@
 // state variables 
 //the three slots will be random int
-let slot1 = 0;
-let slot2 = 0;
-let slot3 = 0;
+let slot1 = randomInt();
+let slot2 = randomInt();
+let slot3 = randomInt();
 // declare a message 
 let msg;
+let spinBtn;
 
 console.log(slot1, slot2, slot3);
 // cached elements
 // the three slots will be assigned to my bubbles on the web browser
-document.getElementById('slot1').innerText = slot1;
-document.getElementById('slot2').innerText = slot2;
-document.getElementById('slot3').innerText = slot3;
+function render() {
+    document.getElementById('slot1').innerText = slot1;
+    document.getElementById('slot2').innerText = slot2;
+    document.getElementById('slot3').innerText = slot3;
+}
 
+render();
 // assign message element 
 document.querySelector('h4').innerText = msg;
 
 //assign button element
-document.querySelector('button') = spinBtn;
+spinBtn = document.querySelector('button')
 
 // assign each int to a img/symbol
 
 
 // event listener
 //click the spin button to play again 
-
-
+function spin() {
+spinBtn.addEventListener('click', init())
+render();
+}
 // functions
 // when you get three matching symbols the message will read Winner!
+function checkWinner() {
+    if (slot1 === slot2 === slot3) {
+        console.log('Winner!')
 // when you don't have three matching symbols the message will read Spin Again!
+    } else {
+        console.log('Spin Again!')
+    }
+}  
+// declare random int for slot variables 
+function randomInt() {
+return Math.ceil(Math.random(1) * 3)
+}
+console.log(randomInt())
